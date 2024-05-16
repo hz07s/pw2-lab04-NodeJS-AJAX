22768871713fd199bc0b6477313361650c4e6cdd
Eduardo <eportugalpor@unsa.edu.pe>
2024-05-15 23:56:00 -0500

    Ruta para obtener la lista de archivos Markdown disponibles

--- ServerMK.js
+++ ServerMK.js
@@ -15,3 +15,19 @@ app.use(bp.urlencoded({
 app.listen(3000, () => {
 	console.log("Escuchando en: http://localhost:3000")
 })
+
+app.get('/', (request, response) => {
+	response.sendFile(path.resolve(__dirname, 'index.html'))
+})
+
+// Ruta para obtener la lista de archivos Markdown disponibles
+app.get('/files', (req, res) => {
+    fs.readdir(path.join(__dirname, 'publi', 'MarkDownFiles'), (err, files) => {
+        if (err) {
+            res.status(500).json({ error: 'Error al leer los archivos' });
+        } else {
+            const markdownFiles = files.filter(file => file.endsWith('.md'));
+            res.json({ files: markdownFiles });
+        }
+    });
+});
