734817bc8d830232df66ea7114544c7757896adb
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2024-05-16 00:18:00 -0500

    funcion addRow para agregar filas a la tabla los archivos md creados

--- publi/client.js
+++ publi/client.js
@@ -2,6 +2,32 @@ document.addEventListener('DOMContentLoaded', () => {
     refreshFileList();
 });
 
+function addRow(name) {
+    const table = document.getElementById('table_list_MD');
+    const row = table.insertRow();
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
+
+    const fileName = name.slice(0, -3);
+    cell1.textContent = fileName;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('d');
+    deleteButton.textContent = 'Borrar';
+    deleteButton.onclick = function() {
+        deleteFile(name);
+    };
+    cell2.appendChild(deleteButton);
+
+    const viewButton = document.createElement('button');
+    viewButton.classList.add('v');
+    viewButton.textContent = 'Visualizar';
+    viewButton.onclick = function() {
+        viewFile(name);
+    };
+    cell2.appendChild(viewButton);
+}
+
 // Obtener y mostrar la lista de archivos
 function refreshFileList() {
     fetch('http://localhost:3000/files')
