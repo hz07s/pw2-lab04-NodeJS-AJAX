0d7f8725680d830780768cbbd0632a1872d0be84
Eduardo <eportugalpor@unsa.edu.pe>
2024-05-15 23:30:00 -0500

    Agregando modulos a script del servidor, haciendo prueba en el puerto 3000

---
+++ ServerMK.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
+const bp = require('body-parser')
+const MarkdownIt = require('markdown-it'),
+	md = new MarkdownIt();
+const app = express()
+
+app.use(express.static('publi'))
+app.use(bp.json())
+app.use(bp.urlencoded({
+	extended: true
+}))
+
+app.listen(3000, () => {
+	console.log("Escuchando en: http://localhost:3000")
+})
